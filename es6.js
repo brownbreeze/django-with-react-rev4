32fd17ea3760dd4e0f10535b38ccbdf5d179277a
brownbreeze <913breeze@gmail.com>
2023-07-02 16:07:56 +0000

    es6 basic usage
    - spread operator

--- es6.js
+++ es6.js
@@ -1,20 +1,11 @@
-const tom = {
-    name: 'Tom',
-    age: 10
+let tom = {
+    name : "Tom",
+    age : 10
 };
 
-const {name, age, height} = tom;
-
-console.log(name);
-console.log(age);
-console.log(height);
-
-const print_person1 = (person) =>{
-    console.log(person.name);
-};
-const print_person2 = ({name}) =>{
-    console.log(name);
-};
-
-print_person1(tom);
-print_person2(tom);
+let steve = {
+    ...tom,
+    name : "Steve"
+}
+console.log(tom);
+console.log(steve);
