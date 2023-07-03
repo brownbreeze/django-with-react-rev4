e0e0002fd48eaffbee8a4cfbad427f7a5762966b
brownbreeze <913breeze@gmail.com>
2023-07-03 14:42:04 +0000

    es6 basic usage
    - callback promise (1)

--- es6.js
+++ es6.js
@@ -1,17 +1,11 @@
-function fn1(x, y) {
-    return x+y;
+const fs = require('fs');
 
-};
+fs.readdir('.', function(err, files){
+    if (err){
+        console.log('error finding files '+ err);
+    }else{
+        console.log(files);
+    }
+});
 
-// X 
-const fn2 = function() {
-    return x+y;
-
-};
-
-// 주로 
-const fn3 = () => (x + y);
-; 
-
-// fn3 변형도 오케이
-const fn4 = x => x + 10;
+console.log('ended');
