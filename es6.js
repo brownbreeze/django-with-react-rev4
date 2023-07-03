d1fac8bbc5b60baea009fa7afe9385cf1b6ea759
brownbreeze <913breeze@gmail.com>
2023-07-03 15:06:03 +0000

    es6 basic usage
    - high order function

--- es6.js
+++ es6.js
@@ -1,11 +1,19 @@
-function Person(name, age){
-    this.name = name;
-    this.age = age; 
+function base_10(fn) {
+    function wrap(x, y){
+        return fn(x, y) + 10;
+    }
+    return wrap;
 }
 
-class Person2{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
+function mysum(a,b){
+    return a+b;
 }
+
+const base_100 = (fn) => (x,y) => fn(x,y)+100;
+    // const wrap = (x,y) => fn(x, y) +100;
+    // return wrap;
+// }
+const mysum2 = (a,b) => a+b;
+
+const result_fn2 = base_100(mysum2);
+console.log(result_fn2(1,2));
