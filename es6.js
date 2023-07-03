0aa651537e11e5c0c57d01fb9e237083b907b453
brownbreeze <913breeze@gmail.com>
2023-07-03 14:48:05 +0000

    es6 basic usage
    - callback -> promise -> async/await(3)
        - es8(ECAM 2017)
        - async

--- es6.js
+++ es6.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const fsPromises = fs.promises; 
 
-fsPromises.readdir('.')
-    .then(files => {
+async function fn() {
+    try {
+        let files = await fsPromises.readdir('.');
         console.log(files);
-    })
-    .catch(err => console.error(err));
-    
+    }catch(err){
+        console.error(err);
+    }
+}
+
+fn();    
 console.log('ended');
