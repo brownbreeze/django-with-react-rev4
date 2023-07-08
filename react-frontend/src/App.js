0eb66a1b524aa8b99b9c6489b3dca6cc5865e50e
brownbreeze <913breeze@gmail.com>
2023-07-08 15:33:56 +0000

    react lesson

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import { Button } from 'antd';
 import './App.css';
-// import 'antd/dist/react.css'
+// import 'antd/dist/react.css' # version 4 후부터 필요 없음
 
 function App() {
   return (
@@ -12,3 +11,7 @@ function App() {
 }
 
 export default App;
+// 부모 컴포넌트로부터 내려받는 속성값 : props
+// 컴포넌트 내부에서 생성/관리되는 상탯값 : state
+// class component는 render() 함수가 호출
+// function component는 그 함수가 매번 호출 -> 유지해야할 값들은 hook을 통해 관리함(hook 얼마안됨)
