bef955921c1b0b92e4427ab797dfd5bfce46828d
brownbreeze <913breeze@gmail.com>
2023-07-08 15:29:24 +0000

    design
    - ant design

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,28 +1,12 @@
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
-
-function TodoList() {
-  const title = "...";
-  const name = "...";
-}
+// import 'antd/dist/react.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary" onClick={()=>console.log('clicked')}>hello buttn</Button>
     </div>
   );
 }
