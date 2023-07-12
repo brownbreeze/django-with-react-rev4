a6d148ec3d59bfa3acb73d02907661fc48432476
brownbreeze <913breeze@gmail.com>
2023-07-12 13:59:59 +0000

    react - component
    - component example
        - plus , minus ball

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,6 +4,15 @@ import React from 'react';
 
 
 class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Counter color="green" />
+        <Counter  />
+        <Counter color="blue" />
+      </div>
+    )
+  }
 
 } 
 
--- react-frontend/src/Counter.js
+++ react-frontend/src/Counter.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 class Counter extends React.Component {
+    static defaultProps = {
+        color : 'red',
+    }
+    state = {
+        color : this.props.color,
+        value : 0,
+    }
+
+    onClick = () => {
+        // this.setState({ value: this.state.value + 1});
+        this.setState(({value : prevValue}) => ({
+            value : prevValue + 1
+        }))
+    }
+
+    onContextMenu = (e) => {
+        e.preventDefault();
+        this.setState(({value : prevValue}) => ({
+            value : (prevValue > 1 ? prevValue - 1 : 0)
+        }))
+    }
+
     render( ){
+        const { color, value } = this.state;
         return (
-            <div>
-                Counter
+            <div onClick={this.onClick} 
+                onContextMenu={this.onContextMenu}
+                style={{...style, backgroundColor: color }}>
+                {value}
             </div>
         )
     }
 }
+const style = {
+    width : '100px',
+    height : '100px',
+    display : 'inline-block',
+    // backgroundColor : 'red',
+    borderRadius : '50px',
+    textAlign : 'center',
+    lineHeight:'100px',
+    userSelet : 'none',
+    fontSize:'3rem',
+    margin : '3rem',
+};
 
 export default Counter;
