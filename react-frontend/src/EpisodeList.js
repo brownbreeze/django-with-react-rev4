9511d8998a98087131ebd8f49df2018ba306d29d
brownbreeze <913breeze@gmail.com>
2023-07-17 13:42:18 +0000

    react lecture
    - not class, function

--- react-frontend/src/EpisodeList.js
+++ react-frontend/src/EpisodeList.js
@@ -3,6 +3,15 @@ import Axios from "axios";
 import episodeApi from "./EpisodeApi";
 import { Card, Col, Row } from "antd";
 
+//class 컴포넌트를 사용하지 않는 이유
+// clss가 코드 재사용성과 코드 구성을 더 어렵게 만든다.
+// this. 는 언어마다 다르며, 이벤트 핸들러가 등록되는 방법을 기억해야만 한다.
+// 부수적으로 작성해야하는 코드가 많다.
+// 서로 연관성없는 다수의 로직을 하나의 생명주기 메서드에서 구현하는 경우가 많다.
+// componentDidMount에서 등록하고 componentWillUnmount에서 해제를 깜빡할 수 있다.
+
+// 해결 방법 -> Hook을 활용해서 함수형 컴포넌트로 해결 가능
+
 class EpisodeDetail extends React.Component {
     state = {
         episode : this.props.episode,
