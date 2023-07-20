e7f1743a44b341e29b44a6942286714b6c79d4bd
brownbreeze <913breeze@gmail.com>
2023-07-20 15:47:35 +0000

    react lecture
    - edit reducer

--- react-frontend/src/App9.js
+++ react-frontend/src/App9.js
@@ -1,52 +1,47 @@
-// import { App } from "antd";
-import React, { createContext, useState, useContext, useReducer } from "react" ;
-
-const INCREMENT = "COUNTER/INCREMENT";
-const DECREMENT = "COUNTER/DECREMENT";
+import React, { useState, createContext, useReducer, useContext } from 'react';
 
 const CounterContext = createContext();
 
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
 const reducer = (prevState, action) => {
-    const { type, payload: value = 1 } = action;
-    if ( type === INCREMENT ){
-        return prevState + value ;
-    } else {
-        return prevState - value;
-    }
-    return prevState;
+  const { type, payload: value = 1 } = action;
+  if (type === INCREMENT) {
+    return prevState + value;
+  } else if (type === DECREMENT) {
+    return prevState - value;
+  }
+  return prevState;
 };
 
-const App9 = () => {
-    const [state, dispatch] = useReducer(reducer , 0);
+const actionIncrement = (value) => ({ type: INCREMENT, payload: value });
+const actionDecrement = (value) => ({ type: DECREMENT, payload: value });
 
-    // const [value, setValue] = useState(0);
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, 0);
 
-    return(
-        <div>
-            <h1 onClick={() => dispatch(actionDecrement())}>App9 {state}</h1>
-            <CounterContext.Provider value={{ state, dispatch }}>
-                <GameBox/>
-            </CounterContext.Provider>
-        </div>
-    )
-
+  return (
+    <div>
+      <h1 onClick={() => dispatch(actionDecrement(10))}>App9 : {state}</h1>
+      <CounterContext.Provider value={{ state, dispatch }}>
+        <GameBox />
+      </CounterContext.Provider>
+    </div>
+  );
 };
-
-const actionIncrement = (value) => ({ payload: value, type:INCREMENT });
-const actionDecrement = (value) => ({ payload: value, type:DECREMENT});
-
 
 const GameBox = () => {
-    const {state: countValue, dispatch} = useContext(CounterContext);
-    const onClick = () =>{
-        dispatch({type:INCREMENT});
-    };
-    return (
-        <div>
-            <h2>Game Box </h2>
-            <button onClick={onClick}>{countValue}</button>
-        </div>
-    )
+  const { state: countValue, dispatch } = useContext(CounterContext);
+  const onClick = () => {
+    dispatch(actionIncrement(5));
+  };
+  return (
+    <div>
+      <h2>GameBox</h2>
+      <button onClick={onClick}>{countValue}</button>
+    </div>
+  );
 };
 
-export default App9;
+export default App;
